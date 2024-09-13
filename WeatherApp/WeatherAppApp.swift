6efbe27adf30f8ee082ffca4842efeaa41348fbb
Justin Jiang <jus3472@gmail.com>
2024-09-13 16:51:27 -0500

    Initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Justin Jiang on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
